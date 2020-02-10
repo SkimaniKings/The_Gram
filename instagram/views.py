c658221c961fe68d19f856cc0f88a118fc595982
skimanikings <kimanisimon856@gmail.com>
2020-02-10 09:26:42 +0300

    import the Userform and the messages to the views

--- instagram/views.py
+++ instagram/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render,HttpResponse
+
+from django.shortcuts import render, redirect
+from .forms import UserReagisterForm
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
