7f748dbf1f5252bf02d5f02076597f051107a5dd
skimanikings <kimanisimon856@gmail.com>
2020-02-10 17:14:20 +0300

    fix a lot bugs and display the loginform

--- instagram/urls.py
+++ instagram/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
-    url('login/', views.login, name='login'),
+    # url('login/', views.login, name='login'),
     url(r'profile/', views.profile, name='profile'),
+    url('home_page/', views.home_page, name='home_page'),
       
 ]
--- instagram/views.py
+++ instagram/views.py
@@ -17,26 +17,18 @@ def home(request):
                 form=UserReagisterForm()
     return render(request, "index.html",{"form":form})
 
-def login(request):
+# def login(request):
 
-    # if request.method == "POST":
-    #     form=UserReagisterForm(request.POST)
-    #     if form.is_valid():
-                
-    #         form.save()
-    #         username = form.cleaned_data.get("username")
-    #         messages.success(request, f("Account for {username} has been created successfully"))
-    #         return redirect("login")
-    #     else:
-    #             form=UserReagisterForm()
-    return render(request, "login.html")
+
+#     return render(request, "login.html")
 
    
-    return render(request, "login.html")
+#     return render(request, "login.html")
 
 
 def profile(request):
     return render(request,"profile.html")
-
+def home_page(request):
+    return render(request,"home.html")
 
 
