2479e2a40891ef1f11f2cbcebdfb33b30d588e4d
skimanikings <kimanisimon856@gmail.com>
2020-02-13 08:07:23 +0300

    create a test to test for saving the user

--- instagram/tests.py
+++ instagram/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from .models import Profile, Image,Post,User
 
 # Create your tests here.
+
+class ImageTest(TestCase):
+    def setUp(self):
+        self.new_user = User(username='simonking', email='simonking@gmail.com', password='mynameiskimani1234')
+        self.new_user.save()
+       
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_user, User))
+        
+        
